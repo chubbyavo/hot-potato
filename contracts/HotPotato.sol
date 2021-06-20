2460197c0613e963e0e40f9f2f0717af963b7c30
chubbyavo <chubbyavo@gmail.com>
2021-06-20 09:16:24 -0700

    Make HotPotato ERC721

--- contracts/HotPotato.sol
+++ contracts/HotPotato.sol
@@ -1,6 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract HotPotato {
-    constructor() {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract HotPotato is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("HotPotato", "HOT") {}
+
+    function safeMint(address to) public {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
 }
--- test/hotPotato.ts
+++ test/hotPotato.ts
@@ -1,17 +1,37 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("HotPotato contract", function () {
   let HotPotato: ContractFactory;
   let hotPotato: Contract;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
 
   beforeEach(async function () {
     HotPotato = await ethers.getContractFactory("HotPotato");
     hotPotato = await HotPotato.deploy();
+    [owner, addr1, addr2] = await ethers.getSigners();
   });
 
-  it("Test", async function () {
-    expect(true).to.be.true;
+  describe("ERC721", function () {
+    it("Should have expected name and symbol", async function () {
+      expect(await hotPotato.name()).to.equal("HotPotato");
+      expect(await hotPotato.symbol()).to.equal("HOT");
+    });
+
+    it("Should mint with auto-incremented id", async function () {
+      hotPotato = await HotPotato.deploy();
+
+      await hotPotato.safeMint(owner.address);
+      expect(await hotPotato.ownerOf(0)).to.equal(owner.address);
+      expect(await hotPotato.balanceOf(owner.address)).to.equal(1);
+
+      await hotPotato.connect(addr1).safeMint(addr2.address);
+      expect(await hotPotato.ownerOf(1)).to.equal(addr2.address);
+      expect(await hotPotato.balanceOf(addr2.address)).to.equal(1);
+    });
   });
 });
