032a495b5038d3c7a4aeaffe7d50e7f646c60ed0
chubbyavo <chubbyavo@gmail.com>
2021-06-20 21:23:01 -0700

    Add basic hotness logic

--- contracts/HotPotato.sol
+++ contracts/HotPotato.sol
@@ -7,12 +7,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract HotPotato is ERC721 {
     using Counters for Counters.Counter;
 
+    uint256 private constant _HOT_DURATION = 1 days;
     Counters.Counter private _tokenIdCounter;
+    mapping(uint256 => uint256) private _lastTouched;
 
     constructor() ERC721("HotPotato", "HOT") {}
 
     function safeMint(address to) public {
         _safeMint(to, _tokenIdCounter.current());
+        _lastTouched[_tokenIdCounter.current()] = block.timestamp;
         _tokenIdCounter.increment();
+    }
+
+    function isHot(uint256 tokenId) public view returns (bool) {
+        uint256 lastTouched = _lastTouched[tokenId];
+        require(lastTouched != 0, "isHot query for nonexistent token");
+        return (block.timestamp - lastTouched) < _HOT_DURATION;
     }
 }
--- test/hotPotato.ts
+++ test/hotPotato.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
@@ -32,6 +32,36 @@ describe("HotPotato contract", function () {
       await hotPotato.connect(addr1).safeMint(addr2.address);
       expect(await hotPotato.ownerOf(1)).to.equal(addr2.address);
       expect(await hotPotato.balanceOf(addr2.address)).to.equal(1);
+    });
+  });
+
+  describe("HotPotato Logic", function () {
+    it("Minted potato should be hot", async function () {
+      hotPotato = await HotPotato.deploy();
+      await hotPotato.safeMint(owner.address);
+      expect(await hotPotato.isHot(0)).to.true;
+    });
+
+    it("Potato is still hot after 23 hours", async function () {
+      hotPotato = await HotPotato.deploy();
+      await hotPotato.safeMint(owner.address);
+      expect(await hotPotato.isHot(0)).to.true;
+
+      await network.provider.send("evm_increaseTime", [3600 * 23]);
+      await network.provider.send("evm_mine");
+
+      expect(await hotPotato.isHot(0)).to.true;
+    });
+
+    it("Potato becomes cold after 1 day", async function () {
+      hotPotato = await HotPotato.deploy();
+      await hotPotato.safeMint(owner.address);
+      expect(await hotPotato.isHot(0)).to.true;
+
+      await network.provider.send("evm_increaseTime", [3600 * 24]);
+      await network.provider.send("evm_mine");
+
+      expect(await hotPotato.isHot(0)).to.false;
     });
   });
 });
