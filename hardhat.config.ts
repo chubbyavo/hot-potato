f6a1cff2da21b18e37cc9244d1c191790250479c
chubbyavo <chubbyavo@gmail.com>
2021-06-29 21:36:33 -0700

    Add test networks in hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import "@nomiclabs/hardhat-waffle";
 import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { ROPSTEN_API_URL, RINKEBY_API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: ROPSTEN_API_URL!,
+      accounts: [PRIVATE_KEY!],
+    },
+    rinkeby: {
+      url: RINKEBY_API_URL!,
+      accounts: [PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
