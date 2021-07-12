9b568f09d7b6bcfe6bf71c94a231ec783ded12ac
chubbyavo <chubbyavo@gmail.com>
2021-07-11 20:23:13 -0700

    Wire the frontend with local hardhat node

--- frontend/src/hooks/useHotPotato.ts
+++ frontend/src/hooks/useHotPotato.ts
@@ -2,8 +2,10 @@ import { useWeb3React } from "@web3-react/core";
 import { useEffect, useState } from "react";
 import { HotPotato, HotPotato__factory } from "../typechain";
 
+// TODO: get address from env
 // const contractAddress = "0x845f8d42AFf6CfFe840DCf0967eD468f530ecAa4"; // ropsten
-const contractAddress = "0xB0F5ebB9C3Dc26CC217088635d2a76FcCCd6AC4f"; // rinkeyby
+// const contractAddress = "0xB0F5ebB9C3Dc26CC217088635d2a76FcCCd6AC4f"; // rinkeyby
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // local
 
 const useHotPotato = (): HotPotato | null => {
   const { active, library } = useWeb3React();
--- frontend/src/utils/connectors.ts
+++ frontend/src/utils/connectors.ts
@@ -3,7 +3,8 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 // const MAINNET_CHAIN_ID = 1;
 // const ROPSTEN_CHAIN_ID = 3;
 const RINKEYBY_CHAIN_ID = 4;
+const HARDHAT_CHAIN_ID = 31337;
 
 export const injectedConnector = new InjectedConnector({
-  supportedChainIds: [RINKEYBY_CHAIN_ID],
+  supportedChainIds: [RINKEYBY_CHAIN_ID, HARDHAT_CHAIN_ID],
 });
